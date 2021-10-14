c4dadc1d012c1fcd6031900e8d0f31a7aa3bd4d3
Jegor Chomczuk <jegor.chomczuk@gmail.com>
2021-10-14 14:17:24 +0200

    Final commit

--- src/main/java/com/EnterpriseJavaDevelopment62/BookFormatService/BookFormatServiceApplication.java
+++ src/main/java/com/EnterpriseJavaDevelopment62/BookFormatService/BookFormatServiceApplication.java
@@ -3,9 +3,11 @@ package com.EnterpriseJavaDevelopment62.BookFormatService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableFeignClients
 public class BookFormatServiceApplication {
 
 	public static void main(String[] args) {
